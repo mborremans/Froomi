c2ee6bf03edccf1e7daeb9be4900ffc35607bd61
mborr <r0799923@student.thomasmore.be>
2021-04-22 09:56:27 +0200

    add initial files

---
+++ src/main/java/thomasmore/froomi/FroomiApplication.java
@@ -0,0 +1,13 @@
+package thomasmore.froomi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FroomiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FroomiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/thomasmore/froomi/FroomiApplicationTests.java
@@ -0,0 +1,13 @@
+package thomasmore.froomi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FroomiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
