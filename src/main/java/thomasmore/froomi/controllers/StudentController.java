4c29dde74a39700f1204fd50511737ab052f4d2d
BsgMax00 <76077545+BsgMax00@users.noreply.github.com>
2021-04-25 17:35:24 +0200

    studentDetails gemaakt geen bootstrap, geen css

--- src/main/java/thomasmore/froomi/controllers/StudentController.java
+++ src/main/java/thomasmore/froomi/controllers/StudentController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import thomasmore.froomi.model.Student;
 import thomasmore.froomi.repositories.StudentRepository;
+
+import java.util.Optional;
 
 @Controller
 public class StudentController {
@@ -18,5 +21,15 @@ public class StudentController {
 
         model.addAttribute("students", students);
         return "studentList";
+    }
+
+    @GetMapping({"/studentDetails", "/studentDetails/{id}"})
+    public String studentDetails(Model model, @PathVariable(required = false) Integer id){
+        Optional<Student> optionalStudents = studentRepository.findById(id);
+
+        if(optionalStudents.isPresent()){
+            model.addAttribute("student", optionalStudents.get());
+        }
+        return "studentDetails";
     }
 }
---
+++ src/main/resources/Data.sql
@@ -0,0 +1,2 @@
+INSERT INTO STUDENT(ID, NAME, STREET, TOWN)
+values(1, 'naam1', 'straat1', 'town1');
